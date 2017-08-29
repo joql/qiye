6aa6f89fac599d85612816a29ea9bb4c5a3a8d07
joql <981037735@qq.com>
2017-08-29 11:25:17 +0800

    1

---
+++ application/common/Base.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\common;
+use think\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/29 0029
+ * Time: 10:46
+ */
+class Base extends Controller{
+
+}
