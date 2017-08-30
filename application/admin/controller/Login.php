6792f0e9434b41a53c76880f85f28894107638da
Joql <ghjoql001>
2017-08-30 23:00:33 +0800

    test

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -5,6 +5,7 @@ namespace app\admin\controller;
 
 use app\common\Base;
 use think\Request;
+use app\admin\model\Admin;
 
 class Login extends Base
 {
@@ -16,8 +17,24 @@ class Login extends Base
         return $this->fetch('login');
     }
 
-    public function login(){
+    public function check(){
+        $status =0;
 
+        $username = $_POST['username'];
+        $pass = $_POST['pass'];
+
+        $map = ['username'=>$username];
+        $admin = Admin::get($map);
+
+        if(is_null($admin)){
+            $msg = '用户名不对';
+        }else{
+            $status =1;
+            $msg = 'success';
+            $admin->setInc('count');
+            $admin->save(['last_time'=>time()]);
+        }
+        return json_encode(array(   'code'=>1,'msg'=>$msg));
 
     }
 
